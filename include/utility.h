7a65c87226c5d362461ac9f31da4b58eb6d0065d
MatthieuHR <matt.hiessler@gmail.com>
2023-12-09 17:58:56 +0100

    update utility.c and configuration.h

--- include/utility.h
+++ include/utility.h
@@ -2,4 +2,7 @@
 
 #include <defines.h>
 
-char *concat_path(char *result, char *prefix, char *suffix);
+char *concat_path(char *result, char *prefix, char *suffix){
+    result = strcat(prefix,suffix);
+    return result;
+}
